d41b8fde7312c775e94a37a7fc20fb45a9a1b1ef
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-19 16:44:40 +0300

    chat practice

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'layout/chat.dart';
 void main() => runApp(Chat());
 
 class Chat extends StatelessWidget {
+  const Chat({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
